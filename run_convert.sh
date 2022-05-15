f2df2a1f155439468abe3f189a401a6d4d4b2105
rehohoho <ernie.birdy.ho@gmail.com>
2022-05-15 18:42:34 +0800

    Bash: add run convert script to go from torch pth to tflite

--- run_convert.sh
+++ run_convert.sh
@@ -1,36 +1,78 @@
-CONFIG_FILE=configs/ocrnet/ocrnet_hr18_512x1024_40k_labelmefacade.py
-CHECKPOINT_FILE=work_dirs/ocrnet_hr18_512x1024_40k_labelmefacade/mlda_d4ddy_bushier_seg.pth
-ONNX_FILE=work_dirs/ocrnet_hr18_512x1024_40k_labelmefacade/mlda_d4ddy_bushier_seg.onnx
-INPUT_IMG=data/labelmefacade/tests/IMG_4303.jpg
-INPUT_SHAPE="3024 4032"
-RESCALE_SHAPE="512 384"
+CONFIG_FILE=configs/ocrnet/ocrnet_hr18_512x1024_20k_labelmefacade.py
+WORK_DIR=work_dirs/ocrnet_hr18_512x1024_20k_labelmefacade
 
-python tools/pytorch2onnx.py \
-    ${CONFIG_FILE} \
-    --checkpoint ${CHECKPOINT_FILE} \
-    --output-file ${ONNX_FILE} \
-    --input-img ${INPUT_IMG} \
-    --shape ${INPUT_SHAPE} \
-    --rescale-shape ${RESCALE_SHAPE} \
-    --show \
-    --verify \
-    --dynamic-export \
-    --cfg-options \
-      model.test_cfg.mode="whole"
+if [ ! -f ${WORK_DIR}/latest.pth ]; then
+    printf "Unable to find pytorch checkpoint ${WORK_DIR}/latest.pth"
+    exit 1
+fi
 
-MODEL_FILE=$ONNX_FILE
-BACKEND=onnxruntime
-OUTPUT_FILE=work_dirs/ocrnet_hr18_512x1024_40k_labelmefacade/onnx_out.pkl
-EVALUATION_METRICS=mIoU
-SHOW_DIRECTORY=work_dirs/ocrnet_hr18_512x1024_40k_labelmefacade/onnx_out_dir
-OPACITY=0.5
+if [ ! -f ${WORK_DIR}/latest.onnx ]; then
+    printf "GENERATING ONNX \n\n\n"
 
-python tools/deploy_test.py \
-    ${CONFIG_FILE} \
-    ${MODEL_FILE} \
-    --backend ${BACKEND} \
-    --out ${OUTPUT_FILE} \
-    --eval ${EVALUATION_METRICS} \
-    --show \
-    --show-dir ${SHOW_DIRECTORY} \
-    --opacity ${OPACITY}
+    INPUT_IMG=data/labelmefacade/tests/IMG_4303.jpg
+    INPUT_SHAPE="384 512"
+    RESCALE_SHAPE="384 512"
+
+    python tools/pytorch2onnx.py \
+        ${CONFIG_FILE} \
+        --checkpoint ${WORK_DIR}/latest.pth \
+        --output-file ${WORK_DIR}/latest.onnx \
+        --input-img ${INPUT_IMG} \
+        --shape ${INPUT_SHAPE} \
+        --show \
+        --verify \
+        --cfg-options \
+        model.test_cfg.mode="whole"
+fi
+
+if false && [ ! -f ${WORK_DIR}/onnx_out.pkl ]; then
+    printf "TESTING ONNX \n\n\n"
+
+    MODEL_FILE=${WORK_DIR}/latest.onnx
+    BACKEND=onnxruntime
+    OUTPUT_FILE=${WORK_DIR}/onnx_out.pkl
+    EVALUATION_METRICS=mIoU
+    SHOW_DIRECTORY=${WORK_DIR}/onnx_out_dir
+    OPACITY=0.5
+
+    python tools/deploy_test.py \
+        ${CONFIG_FILE} \
+        ${MODEL_FILE} \
+        --backend ${BACKEND} \
+        --out ${OUTPUT_FILE} \
+        --eval ${EVALUATION_METRICS} \
+        --show \
+        --show-dir ${SHOW_DIRECTORY} \
+        --opacity ${OPACITY}
+fi
+
+if [ ! -d ${WORK_DIR}/latest.pb ]; then
+    printf "GENERATING PB \n\n\n"
+    python tools/onnx2tf.py --onnx_in ${WORK_DIR}/latest.onnx --tf_out ${WORK_DIR}/latest.pb
+fi
+
+if [ ! -f ${WORK_DIR}/latest.tflite ]; then
+    printf "GENERATING TFLITE \n\n\n"
+    python tools/tf2tflite.py --tf_in ${WORK_DIR}/latest.pb --tflite_out ${WORK_DIR}/latest.tflite
+fi
+
+if [ ! -f ${WORK_DIR}/tflite_out.pkl ]; then
+    printf "TESTING TFLITE \n\n\n"
+
+    MODEL_FILE=${WORK_DIR}/latest.tflite
+    BACKEND=tflite
+    OUTPUT_FILE=${WORK_DIR}/tflite_out.pkl
+    EVALUATION_METRICS=mIoU
+    SHOW_DIRECTORY=${WORK_DIR}/tflite_out_dir
+    OPACITY=0.5
+
+    python tools/deploy_test.py \
+        ${CONFIG_FILE} \
+        ${MODEL_FILE} \
+        --backend ${BACKEND} \
+        --out ${OUTPUT_FILE} \
+        --eval ${EVALUATION_METRICS} \
+        --show \
+        --show-dir ${SHOW_DIRECTORY} \
+        --opacity ${OPACITY}
+fi
