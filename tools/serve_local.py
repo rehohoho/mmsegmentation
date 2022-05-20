3eea37a11c96f71a279b1af196434d8ee142dac7
rehohoho <ernie.birdy.ho@gmail.com>
2022-05-20 21:57:36 +0800

    serve_local: add TfliteModelServer

--- tools/serve_local.py
+++ tools/serve_local.py
@@ -3,9 +3,14 @@ Serving class that consumes images and outputs segmentation bitmap.
 """
 
 import numpy as np
+import torch
 import mmcv
 
-from mmseg.apis.inference import init_segmentor, inference_segmentor
+from mmcv.parallel import collate
+from mmseg.datasets.pipelines import Compose
+from mmseg.apis.inference import init_segmentor, inference_segmentor, LoadImage
+from mmseg.ops import resize
+from deploy_test import TfliteSegmentor
 
 
 class LocalModelServer:
@@ -28,17 +33,50 @@ class LocalModelServer:
   def get_visualise(self, img: np.ndarray, result: np.ndarray):
     return self.model.show_result(img, result, opacity=0.5)
 
+class TfliteModelServer:
+    def __init__(self, config_file: str, checkpoint_file: str):
+        self.cfg = mmcv.Config.fromfile(config_file)
+        self.model = TfliteSegmentor(checkpoint_file, cfg=self.cfg, device_id=0)
+    
+    def infer(self, img: np.ndarray, opacity: float = 0.5):
+        test_pipeline = [LoadImage()] + self.cfg.data.test.pipeline[1:]
+        test_pipeline = Compose(test_pipeline)
+        data = dict(img=img)
+        data = test_pipeline(data)
+        data_img = collate([data['img']], samples_per_gpu=1)[0]
+        data_img = resize(data_img, size=(384, 512), mode='bilinear',align_corners=True, warning=False)
+        seg_pred = self.model.simple_test(data_img, data['img_metas'][0], is_resize=False)
+        
+        seg_pred = torch.from_numpy(seg_pred[0]).float().unsqueeze(0).unsqueeze(0)
+        seg_pred = resize(seg_pred, size=tuple(img.shape[:2]), mode='nearest').squeeze().squeeze()
+        seg = seg_pred.long().detach().cpu().numpy()
+        return seg
+
+    def save_visualise(self, img: np.ndarray, seg: np.ndarray, out_file: str, opacity: float = 0.5):
+        palette = np.array(self.cfg.PALETTE)
+        color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
+        for label, color in enumerate(palette):
+            color_seg[seg == label, :] = color
+        color_seg = color_seg[..., ::-1]
+
+        vis = img * (1 - opacity) + color_seg * opacity
+        vis = vis.astype(np.uint8)
+        mmcv.imwrite(vis, out_file)
 
 # for testing only
 if __name__ == '__main__':
 
-  IMG_FILE='data/labelme_facade/tests/20211016_150104.jpg'
-  CONFIG_FILE='configs/ocrnet/ocrnet_hr18_512x1024_40k_labelmefacade.py'
-  CHECKPOINT_FILE='work_dirs/ocrnet_hr18_512x1024_40k_labelmefacade/latest.pth'
+  IMG_FILE='data/labelmefacade/tests/Picture2.png'
+  CONFIG_FILE='configs/ocrnet/ocrnet_hr18_512x1024_80k_labelmefacade.py'
+  CHECKPOINT_FILE='../bushierbrows.tflite'
   
-  server = LocalModelServer(config_file=CONFIG_FILE, checkpoint_file=CHECKPOINT_FILE)
+  if CHECKPOINT_FILE.endswith('tflite'):
+    server = TfliteModelServer(config_file=CONFIG_FILE, checkpoint_file=CHECKPOINT_FILE)
+  else:
+    server = LocalModelServer(config_file=CONFIG_FILE, checkpoint_file=CHECKPOINT_FILE)  
   img = mmcv.imread(IMG_FILE)
   result = server.infer(img)
+  print(result)
   out_file = IMG_FILE[:-4] + '_result.jpg'
   server.save_visualise(img, result, out_file)
-  # mmcv.imwrite(result[0], IMG_FILE[:-4] + '_result_bitmap.jpg')
+  mmcv.imwrite(result[0], IMG_FILE[:-4] + '_result_bitmap.jpg')
