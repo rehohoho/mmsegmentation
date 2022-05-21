a73ae7a421e07741fda62c9d81b335cbc4b7f7d6
rehohoho <ernie.birdy.ho@gmail.com>
2022-05-21 22:36:01 +0800

    Update configs for hackerfest challenge.

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=$(pwd)
 CONFIG_FILE=configs/ocrnet/ocrnet_hr18_512x1024_80k_labelmefacade.py
 GPU_NUM=1
-CHECKPOINT=work_dirs/ocrnet_hr18_512x1024_80k_labelmefacade/mlda_d4ddy_bushier_seg.pth
+CHECKPOINT=work_dirs/ocrnet_hr18_512x1024_80k_labelmefacade/latest.pth
 OUTPUT=work_dirs/ocrnet_hr18_512x1024_80k_labelmefacade/tests
 
 python tools/test.py ${CONFIG_FILE} ${CHECKPOINT} \
